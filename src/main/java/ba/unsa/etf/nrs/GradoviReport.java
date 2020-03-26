1057533ff8b84022f3092a813751c9cb42ffcbdb
mgojak1 <mgojak1@etf.unsa.ba>
2020-03-26 18:18:17 +0100

    Zavrsen zadatak 3 -- Reports ne radi preko jar

--- src/main/java/ba/unsa/etf/nrs/GradoviReport.java
+++ src/main/java/ba/unsa/etf/nrs/GradoviReport.java
@@ -11,7 +11,7 @@ import java.util.HashMap;
 public class GradoviReport extends JFrame {
     public void showReport(Connection conn) throws JRException {
         String reportSrcFile = getClass().getResource("/reports/gradovi.jrxml").getFile();
-        String reportsDir = getClass().getResource("/reports/").getFile();
+        String reportsDir = getClass().getResource("/reports").getFile();
         JasperReport jasperReport = JasperCompileManager.compileReport(reportSrcFile);
         // Fields for resources path
         HashMap<String, Object> parameters = new HashMap<String, Object>();
